2c9616d690bcebc10bc42a8b8fa15c669f1651e0
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2023-02-26 21:11:03 +0530

    Added solution - LeetHub

---
+++ Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.cpp
@@ -0,0 +1,189 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(){
+        data = 0;
+        left = right = NULL;
+    }
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+// } Driver Code Ends
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution
+{
+    public:
+    //Function to return the lowest common ancestor in a Binary Tree.
+    void fun(Node *root,map<int,vector<Node *>> &m,vector<Node *> v){
+        if(root==NULL) return;
+        
+        v.push_back(root);
+        m[root->data]=v;
+        fun(root->left,m,v);
+        fun(root->right,m,v);
+        v.pop_back();
+        
+    }
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       //Your code here 
+       map<int,vector<Node *>>m;
+       vector<Node *>v;
+       fun(root,m,v);
+    //   for(auto it:m){
+    //       for(auto it2:it.second){
+    //           cout<<it.first<<"->"<<it2->data<<endl;
+    //       }
+    //   }
+    
+    vector<Node *>v1=m[n1];
+    vector<Node *>v2=m[n2];
+    int a=v1.size();
+    int b=v2.size();
+    
+    for(int i=a-1;i>=0;i--){
+        for(int j=b-1;j>=0;j--){
+            if(v1[i]->data==v2[j]->data){
+                return v2[j];
+            }
+        }
+    }
+       
+       
+    }
+    //4 9
+    //1 2 3 4 5 6 7 N N 8 9 N N 10
+};
+
+//{ Driver Code Starts.
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // for(string i:ip)
+    //     cout<<i<<" ";
+    // cout<<endl;
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Function for Inorder Traversal
+void printInorder(Node* root)
+{
+    if(!root)
+        return;
+
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b;
+        scanf("%d %d ",&a,&b);
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout<<ob.lca(root,a,b)->data<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
