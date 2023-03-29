832497000208da74809ad9dcedc081c53457df15
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2023-03-29 11:22:02 +0530

    Added solution - LeetHub

---
+++ Linked List in Zig-Zag fashion - GFG/linked-list-in-zig-zag-fashion.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void print(Node *root)
+{
+	Node *temp = root;
+	while(temp!=NULL)
+	{
+		cout<<temp->data<<" ";
+		temp=temp->next;
+	}
+}
+ 
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+
+class Solution
+{
+    public:
+    Node *zigZag(Node* head)
+    {
+     // your code goes here
+      // code here
+      vector<int>arr;
+        Node *t=head;
+        int f=1;
+        while(t->next){
+            if(f==1 and t->data>t->next->data)
+            {
+            swap(t->data,t->next->data);  
+            }
+            else if(f==0 and  t->data<t->next->data)
+            {
+            swap(t->data,t->next->data); 
+            }
+            if(f==1) f=0;
+            else f=1;
+            t=t->next;
+        }
+        
+        return head;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		Solution ob;
+		Node *ans = ob.zigZag(head);
+		print(ans);
+		cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
