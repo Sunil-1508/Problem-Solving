b22d2197e558b6e7bfa66b46f674c83cd775cf92
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2023-06-22 10:05:21 -0400

    Added solution - LeetHub

--- Lemonade Change - GFG/lemonade-change.cpp
+++ Lemonade Change - GFG/lemonade-change.cpp
@@ -6,27 +6,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    bool lemonadeChange(int n, vector<int> &b) {
+    bool lemonadeChange(int N, vector<int> &bills) {
         // code here
-        map<int,int>m;
-        if(b[0]==10 or b[0]==20) return false;
-        m[b[0]]++;
-        for(int i=1;i<n;i++){
-            if(b[i]==5){
-                m[b[i]]++;
+        map<int,int>mp;
+        for(int i=0;i<N;i++){
+            if(bills[i]==5){
+                mp[bills[i]]++;
             }
-            else if(b[i]==10){
-                if(m[5]==0) return false;
-                else{ m[5]--; m[10]++; }
-                
+            else if(bills[i]==10){
+                if(mp[5]!=0){
+                    mp[5]--;
+                }
+                else{
+                    return false;
+                }
+                mp[bills[i]]++;
             }
             else{
-                if(m[10]>=1 and m[5]>=1){
-                    m[10]--;
-                    m[5]--;
+                if(mp[10]!=0 and mp[5]!=0){
+                    mp[10]--;
+                    mp[5]--;
                 }
-                else if(m[5]>=3){
-                    m[5]-=3;
+                else if(mp[5]>=3){
+                    mp[5]-=3;
                 }
                 else{
                     return false;
@@ -34,7 +36,6 @@ class Solution {
             }
         }
         return true;
-        
     }
 };
 
