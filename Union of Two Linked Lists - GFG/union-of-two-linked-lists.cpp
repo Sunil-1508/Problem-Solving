f1952b13381441f247425e03f9321136b625ec79
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2023-06-27 09:50:22 +0530

    Added solution - LeetHub

---
+++ Union of Two Linked Lists - GFG/union-of-two-linked-lists.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+
+// } Driver Code Ends
+/*
+// structure of the node is as follows
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+class Solution
+{
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        // code here
+        map<int,int>m;
+        while(head1!=NULL){
+            if(m[head1->data]==0) m[head1->data]++;
+            head1=head1->next;
+        }
+        while(head2!=NULL){
+            if(m[head2->data]==0) m[head2->data]++;
+            head2=head2->next;
+        }
+        Node *ans=new Node(10);
+        Node *t=ans;
+        for(auto i : m){
+            ans->next =new Node(i.first);
+            ans=ans->next;
+        }
+        return t->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution obj;
+        Node* head = obj.makeUnion(first,second);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
