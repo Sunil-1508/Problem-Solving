960a7e5a0a9472a2571958d3b8b5b669770f36f6
Sunil <84443462+Sunil-1508@users.noreply.github.com>
2023-06-30 11:59:16 +0530

    Added solution - LeetHub

---
+++ Rearrange a linked list - GFG/rearrange-a-linked-list.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    void rearrangeEvenOdd(Node *head)
+    {
+       if(head==NULL) return;
+       // Your Code here
+       Node *p1=head;
+       Node  *p2=p1->next;
+       Node *f=p2;
+       
+       while(p1->next!=NULL and p2->next!=NULL){
+           p1->next=p1->next->next;
+           p2->next=p2->next->next;
+           p1=p1->next;
+           p2=p2->next;
+       }
+       p1->next=f;
+       
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,i,n,l;
+
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL;
+        struct Node *temp = head;
+        cin>>n;
+        for(i=1; i<=n; i++)
+        {
+            cin>>l;
+
+            if (head == NULL)
+            {   
+                head = temp = new Node(l); 
+                
+            }
+            else
+            {  temp->next = new Node(l);
+               temp = temp->next;
+            }
+        }
+        Solution ob;
+        ob.rearrangeEvenOdd(head);
+        while(head != NULL)
+        {
+            printf("%d ", head->data);
+            head = head->next;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
